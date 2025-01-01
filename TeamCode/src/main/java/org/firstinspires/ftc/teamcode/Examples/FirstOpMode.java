0de6d939dc767535fb2a1fd6202d9f7c5c9dcb37
AlonSchwierz <alon.schwierz@gmail.com>
2025-01-01 17:10:39 +0200

    Final touches before branching

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Examples/FirstOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Examples/FirstOpMode.java
@@ -1,11 +1,8 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Examples;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
-import org.firstinspires.ftc.robotcontroller.external.samples.BasicOpMode_Iterative;
 
 @TeleOp
 public class FirstOpMode extends LinearOpMode {
