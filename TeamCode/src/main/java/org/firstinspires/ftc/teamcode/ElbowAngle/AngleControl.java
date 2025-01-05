824b6a97118764630a8006356b18e0157cd89b4b
AlonSchwierz <goni.shahaf@gmail.com>
2025-01-05 19:13:16 +0200

    this code is a starting code of the degree of the lift the loop is what needs working

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ElbowAngle/AngleControl.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.ElbowAngle;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class AngleControl extends OpMode {
+
+    private DcMotor angleControl;
+
+    @Override
+    public void init() {
+        angleControl = hardwareMap.get(DcMotor.class, "angleControl");
+    }
+
+    @Override
+    public void loop() {
+        boolean upDegreeControl = gamepad1.dpad_up;
+        boolean downDegreeControl = gamepad1.dpad_down;
+        if (upDegreeControl == true) {
+            angleControl.setPower(0.6);
+        } else if (downDegreeControl == false) {
+            angleControl.setPower(-0.6);
+        }
+    }
+}
