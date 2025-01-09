be941585688fa9e976af4a2227f3143dc5c35a4f
AlonSchwierz <bordnoga@gmail.com>
2025-01-09 16:02:18 +0200

    gripper angle control in progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gripper/Gripper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gripper/Gripper.java
@@ -10,71 +10,25 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 @TeleOp
 
 public class Gripper extends OpMode {
-    private ServoEx gripperCatcherServo;
-//    private ServoEx gripperAngleServo;
-    double gamepadStickPlace = 0;
+//    private ServoEx gripperCatcherServo;
+    private ServoEx gripperAngleServo;
+
 
     @Override
     public void init() {
-        gripperCatcherServo = new SimpleServo(hardwareMap, "catcher", 0, 360, AngleUnit.DEGREES);
-//        gripperAngleServo = new SimpleServo(hardwareMap, "angle", 0, 270, AngleUnit.DEGREES);
+//        gripperCatcherServo = new SimpleServo(hardwareMap, "catcher", 0, 360, AngleUnit.DEGREES);
+        gripperAngleServo = new SimpleServo(hardwareMap, "angle", 0, 360, AngleUnit.DEGREES);
+
     }
 
     @Override
     public void loop() {
-        if(gamepad1.a) {
-            gripperCatcherServo.rotateByAngle(10);
-        }
-        else if (gamepad1.b){
-            gripperCatcherServo.rotateByAngle(-10);
-        }
-        else if(gamepad1.y){
-           gripperCatcherServo.rotateByAngle(0.002);
-        }
-
-
-//        if (gamepad1.right_stick_y == 1){
-//            gripperAngleServo.setPosition(1);
-//        }if(gamepad1.right_stick_y == 0.9){
-//            gripperAngleServo.setPosition(0.95);
-//        }if(gamepad1.right_stick_y == 0.8){
-//            gripperAngleServo.setPosition(0.90);
-//        }if(gamepad1.right_stick_y == 0.7){
-//            gripperAngleServo.setPosition(0.85);
-//        }if(gamepad1.right_stick_y == 0.6){
-//            gripperAngleServo.setPosition(0.80);
-//        }if(gamepad1.right_stick_y == 0.5){
-//            gripperAngleServo.setPosition(0.75);
-//        }if(gamepad1.right_stick_y == 0.4){
-//            gripperAngleServo.setPosition(0.70);
-//        }if(gamepad1.right_stick_y == 0.3){
-//            gripperAngleServo.setPosition(0.65);
-//        }if(gamepad1.right_stick_y == 0.2){
-//            gripperAngleServo.setPosition(0.60);
-//        }if(gamepad1.right_stick_y == 0.1){
-//            gripperAngleServo.setPosition(0.55);
-//        }if(gamepad1.right_stick_y == 0){
-//            gripperAngleServo.setPosition(0.50);
-//        }if(gamepad1.right_stick_y == -0.1){
-//            gripperAngleServo.setPosition(0.45);
-//        }if(gamepad1.right_stick_y == -0.2){
-//          gripperAngleServo.setPosition(0.40);
-//        }if(gamepad1.right_stick_y == -0.3){
-//            gripperAngleServo.setPosition(0.35);
-//        }if(gamepad1.right_stick_y == -0.4){
-//            gripperAngleServo.setPosition(0.30);
-//        }if(gamepad1.right_stick_y == -0.5){
-//            gripperAngleServo.setPosition(0.25);
-//        }if(gamepad1.right_stick_y == -0.6){
-//            gripperAngleServo.setPosition(0.20);
-//        }if(gamepad1.right_stick_y == -0.7){
-//            gripperAngleServo.setPosition(0.15);
-//        }if(gamepad1.right_stick_y == -0.8){
-//            gripperAngleServo.setPosition(0.10);
-//        }if(gamepad1.right_stick_y == -0.9){
-//            gripperAngleServo.setPosition(0.05);
-//        }if(gamepad1.right_stick_y == -1){
-//            gripperAngleServo.setPosition(0);
+//        if(gamepad1.a){
+//            gripperCatcherServo.turnToAngle(360);
 //        }
+//        if(gamepad1.b){
+//            gripperCatcherServo.turnToAngle(0);
+//        }
+        gripperAngleServo.setPosition(gamepad1.right_stick_y);
     }
 }
