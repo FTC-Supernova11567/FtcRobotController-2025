aed3cccce3fffb89203e93d86374d8874d032051
AlonSchwierz <bordnoga@gmail.com>
2025-01-09 18:35:01 +0200

    Create arm command

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm/ArmCommands/ExtensionCommand.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.Arm.ArmCommands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+public class ExtensionCommand extends CommandBase {
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override
+    public boolean isFinished() {
+    }
+}
