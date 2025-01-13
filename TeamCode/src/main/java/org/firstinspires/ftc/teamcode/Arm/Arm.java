f13b87a2e3b092d893e59478f62527cba08ad5e4
AlonSchwierz <alon.schwierz@gmail.com>
2025-01-13 21:29:03 +0200

    SystemBased coding

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm/Arm.java
@@ -3,38 +3,57 @@ package org.firstinspires.ftc.teamcode.Arm;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
 import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Arm{
     private Motor extension;
     private Motor angleControl;
+    private Gamepad myGamepad;
 
-    public Arm() {
-        extension = new Motor(hardwareMap, "extension");
-        angleControl = new Motor(hardwareMap, "degree");
+    public Arm(HardwareMap constHardwareMap, Gamepad constMyGamePad) {
+        extension = new Motor(constHardwareMap, "extension");
+        angleControl = new Motor(constHardwareMap, "angleControl");
+        myGamepad = constMyGamePad;
+
     }
 
-    int getCurrentPosition() {
-        return extension.getCurrentPosition();
-    }
+//    int getCurrentPosition() {
+//        return extension.getCurrentPosition();
+//    }
 
-    Motor getExtension() {
-        return extension;
-        //TODO: Continue extension motor
-    }
+//    Motor getExtension() {
+//        return extension;
+//        //TODO: Continue extension motor
+//    }
 
     public void setExtension() {
         extension.setRunMode(Motor.RunMode.RawPower);
-        extension.set(0.6);
+        extension.set(0.7);
+    }
+
+    public void setMinusExtension() {
+        extension.setRunMode(Motor.RunMode.RawPower);
+        extension.set(-0.8);
     }
 
     void setDegree() {
         angleControl.setRunMode(Motor.RunMode.RawPower);
-        extension.set(0.6);
+        angleControl.set(0.6);
+    }
+
+    void setMinusDegree(){
+        angleControl.setRunMode(Motor.RunMode.RawPower);
+        angleControl.set(-0.6);
     }
 
     public void stopExtension() {
         extension.set(0);
     }
 
+
+    public void stopAngleControl() {
+        angleControl.set(0);
+    }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm/ArmNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm/ArmNew.java
@@ -1,17 +1,42 @@
 package org.firstinspires.ftc.teamcode.Arm;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+@TeleOp
 public class ArmNew extends OpMode {
-    private DcMotor angleControl;
-    private DcMotor extension;
+    Arm myArm;
+
     @Override
     public void init() {
-    }
+        myArm = new Arm(hardwareMap, gamepad1);
+    } 
 
     @Override
     public void loop() {
-
+        if (gamepad1.right_trigger != 0) {
+            myArm.setExtension();
+        } else if (gamepad1.right_trigger == 0) {
+            myArm.stopExtension();
+        }
+        if (gamepad1.right_bumper == true) {
+            myArm.setMinusExtension();
+        }
+        else if(gamepad1.right_bumper == false){
+            myArm.stopExtension();
+        }
+       if(gamepad1.dpad_up== true){
+           myArm.setDegree();
+       }
+       else if(gamepad1.dpad_up== false){
+           myArm.stopAngleControl();
+       }
+       if(gamepad1.dpad_down== true){
+           myArm.setMinusDegree();
+       }
+       else if (gamepad1.dpad_down== false) {
+           myArm.stopAngleControl();
+       }
     }
 }
+
