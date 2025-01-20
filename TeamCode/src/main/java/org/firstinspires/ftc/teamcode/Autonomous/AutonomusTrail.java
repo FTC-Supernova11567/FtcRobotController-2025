e4510c398f4718645ce1d07a62f262064a3de72d
AlonSchwierz <alon.schwierz@gmail.com>
2025-01-20 18:00:25 +0200

    Commenting the non-functional systems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomusTrail.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomusTrail.java
@@ -14,59 +14,61 @@ public class AutonomusTrail extends LinearOpMode {
         Arm myArm = new Arm(hardwareMap, gamepad2);
         Gripper myGripper = new Gripper(hardwareMap, gamepad2);
         MecanumDrive mecanum = new MecanumDrive(hardwareMap, gamepad1);
-        ;
-        while (getRuntime() > 0 && getRuntime() < 2) {
-            mecanum.forward();
-        }
-        while (getRuntime() > 2 && getRuntime() < 4) {
-            mecanum.backward();
-        }
-        while (getRuntime() > 4 && getRuntime() < 6) {
-            mecanum.right();
-        }
-        while (getRuntime() > 6 && getRuntime() < 8) {
-            mecanum.left();
-        }
-        while (getRuntime() > 8 && getRuntime() < 10) {
-            mecanum.rotateRight();
-        }
-        while (getRuntime() > 10 && getRuntime() < 12) {
-            mecanum.rotateLeft();
-        }
-        while (getRuntime() > 12 && getRuntime() < 14) {
-            mecanum.diagonalFrontRight();
-        }
-        while (getRuntime() > 14 && getRuntime() < 16) {
-            mecanum.diagonalFrontLeft();
-        }
-        while (getRuntime() > 16 && getRuntime() < 18) {
-            mecanum.diagonalBackRight();
-        }
-        while (getRuntime() > 18 && getRuntime() < 20) {
-            mecanum.diagonalBackLeft();
-        }
 
-        while (getRuntime() > 20 && getRuntime() < 22) {
-            myArm.extend();
-        }
-        while (getRuntime() > 22 && getRuntime() < 24) {
-            myArm.retract();
-        }
+            while (getRuntime() > 0 && getRuntime() < 2) {
+                mecanum.forward();
+            }
+            while (getRuntime() > 2 && getRuntime() < 4) {
+                mecanum.backward();
+            }
+            while (getRuntime() > 4 && getRuntime() < 6) {
+                mecanum.right();
+            }
+            while (getRuntime() > 6 && getRuntime() < 8) {
+                mecanum.left();
+            }
+            while (getRuntime() > 8 && getRuntime() < 10) {
+                mecanum.rotateRight();
+            }
+            while (getRuntime() > 10 && getRuntime() < 12) {
+                mecanum.rotateLeft();
+            }
+            while (getRuntime() > 12 && getRuntime() < 14) {
+                mecanum.diagonalFrontRight();
+            }
+            while (getRuntime() > 14 && getRuntime() < 16) {
+                mecanum.diagonalFrontLeft();
+            }
+            while (getRuntime() > 16 && getRuntime() < 18) {
+                mecanum.diagonalBackRight();
+            }
+            while (getRuntime() > 18 && getRuntime() < 20) {
+                mecanum.diagonalBackLeft();
+            }
 
-        while(getRuntime()>24&& getRuntime()<26){
-            myArm.angleUp();
-        }
-        while (getRuntime()>24&&getRuntime()<26){
-            myArm.angleDown();
-        }
 
-        while (getRuntime()>26&&getRuntime()<28){
-            myGripper.spinForward();
-        }
 
-        while (getRuntime()>28&&getRuntime()<30){
-            myGripper.spinBackward();
-        }
+//        while (getRuntime() > 20 && getRuntime() < 22) {
+//            myArm.extend();
+//        }
+//        while (getRuntime() > 22 && getRuntime() < 24) {
+//            myArm.retract();
+//        }
+//
+//        while(getRuntime()>24&& getRuntime()<26){
+//            myArm.angleUp();
+//        }
+//        while (getRuntime()>24&&getRuntime()<26){
+//            myArm.angleDown();
+//        }
+//
+//        while (getRuntime()>26&&getRuntime()<28){
+//            myGripper.spinForward();
+//        }
+//
+//        while (getRuntime()>28&&getRuntime()<30){
+//            myGripper.spinBackward();
+//        }
 
     }
 }
