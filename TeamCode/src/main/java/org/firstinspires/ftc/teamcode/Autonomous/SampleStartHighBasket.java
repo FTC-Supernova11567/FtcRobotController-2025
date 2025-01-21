d9a5eea54643401351730d06343a722719efa3d0
AlonSchwierz <alon.schwierz@gmail.com>
2025-01-21 17:50:37 +0200

    Working first part of autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SampleStartHighBasket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SampleStartHighBasket.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Arm.Arm;
 import org.firstinspires.ftc.teamcode.DriveTrain.MecanumDrive;
+import org.firstinspires.ftc.teamcode.Gripper.Gripper;
+
 @Autonomous
 public class SampleStartHighBasket extends LinearOpMode {
     // There is no arm extension and no gripper in this code.
@@ -12,33 +14,46 @@ public class SampleStartHighBasket extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         Arm myArm = new Arm(hardwareMap, gamepad2);
         MecanumDrive mecanum = new MecanumDrive(hardwareMap, gamepad2);
+        Gripper gripper = new Gripper(hardwareMap, gamepad2);
+
         while (getRuntime() > 0 && getRuntime() < 0.9) {
             mecanum.forward();
             myArm.angleUp();
         }
+
         while (getRuntime() > 0.9 && getRuntime() < 1.8) {
             mecanum.rotateLeft();
             myArm.angleUp();
         }
-        while (getRuntime() > 1.8 && getRuntime() < 2.1) {
+
+        while (getRuntime() > 1.8 && getRuntime() < 2.2) {
             mecanum.right();
             myArm.angleUp();
         }
-        while (getRuntime() > 2.05 && getRuntime() < 3.2) {
+
+        while (getRuntime() > 2.2 && getRuntime() < 3.3) {
             mecanum.forward();
             myArm.angleUp();
         }
-        mecanum.stop();
-        while (getRuntime() > 0 && getRuntime() < 4.1){
+
+        while (getRuntime() > 3.3 && getRuntime() < 4){
+            mecanum.stop();
             myArm.angleUp();
         }
-//        while(getRuntime() > 4.1 && getRuntime() < 8.1){
-//            mecanum.stop();
-//        }
-        //TODO: extension
-        while(getRuntime() > 8.1 && getRuntime() < 9.25){
-            mecanum.backward();
-            myArm.angleDown();
+
+        while (getRuntime() > 4 && getRuntime() < 6.1){
+            myArm.stopAngle();
+            myArm.extend();
         }
-    }
+
+        while (getRuntime() >6.1 && getRuntime() < 8){
+            myArm.stopExtension();
+            gripper.spinBackward();
+        }
+
+//        while(getRuntime() > 8 && getRuntime() <){
+//         TODO: write backwords driving and arm retraction   
+//        }
+
+}
 }
