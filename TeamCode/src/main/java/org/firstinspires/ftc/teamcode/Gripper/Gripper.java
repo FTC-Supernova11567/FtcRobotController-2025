7c351121822e7fed8c1ebd067cc18954241866fc
YoavHirsch <hirschyoav9@gmail.com>
2025-02-05 22:21:15 +0200

    Improving the limits, and created presets angles for automatic placement of game pieces

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gripper/Gripper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gripper/Gripper.java
@@ -18,7 +18,7 @@ public class Gripper {
 
     public Gripper(HardwareMap constHardwareMap, Gamepad constCatcherGamepad, Gamepad constAngleGamepad) {
         gripperCatcherServo = new CRServo(constHardwareMap, "catcher");
-        gripperAngleServo = new SimpleServo(constHardwareMap, "angle", 30 , 50, AngleUnit.DEGREES);
+        gripperAngleServo = new SimpleServo(constHardwareMap, "angle", 33 , 48, AngleUnit.DEGREES);
 
         catcherGamepad = constCatcherGamepad;
         angleGamepad = constAngleGamepad;
@@ -35,6 +35,9 @@ public class Gripper {
 
     public double getPosition() {
         return gripperAngleServo.getPosition();
+    }
+    public double getAngle() {
+        return gripperAngleServo.getAngle();
     }
 
     public void spinForward(){
@@ -85,18 +88,27 @@ public class Gripper {
 
     //public void turnBBackwards() {
 
+    public void changeAngle(double angle){
+        gripperAngleServo.turnToAngle(angle);
+    }
     public void angleJoystick() {
-        if (angleGamepad.right_stick_y < 0){
-            gripperAngleServo.rotateBy(-0.03);
-        }else if(angleGamepad.right_stick_y > 0){
-            gripperAngleServo.rotateBy(0.03);
+        if (angleGamepad.x){
+            changeAngle(40); // Best angle to drop game piece in lower basket and high basket - front robot
+        }
+        else if(angleGamepad.a){
+            changeAngle(35.5); // Best angle to drop game piece in high basket - behind robot, and collect from ground
+        }
+        else if (angleGamepad.right_stick_y < 0){
+            gripperAngleServo.rotateByAngle(-0.3);
+        }
+        else if(angleGamepad.right_stick_y > 0){
+            gripperAngleServo.rotateByAngle(0.3);
         }
         else if (angleGamepad.right_stick_y == 0){
-            gripperAngleServo.rotateBy(0);
+            gripperAngleServo.rotateByAngle(0);
         }
-        if (angleGamepad.x){
-            gripperAngleServo.turnToAngle(40);
-        }
+
+
 
     }
     public void angleRoatateUp(){
